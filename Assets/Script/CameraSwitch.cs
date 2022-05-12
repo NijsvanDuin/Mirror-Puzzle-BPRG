d48ea277d1ac9203b01e0151e165d61002a66122
333736 <90184972+NMbo@users.noreply.github.com>
2022-05-12 09:59:25 +0200

    Camera switch gemaakt

---
+++ Assets/Script/CameraSwitch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public Camera Camera1;
+    public Camera Camera2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera1.enabled = true;
+        Camera2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Camera1.enabled = !Camera1.enabled;
+            Camera2.enabled = !Camera2.enabled;
+        }
+    }
+    public void CameraSwitchTest()
+    {
+        Camera1.enabled = !Camera1.enabled;
+        Camera2.enabled = !Camera2.enabled;
+    }
+}
